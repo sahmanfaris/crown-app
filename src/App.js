76a257963632018801ea01c13b4c9e3ea974b1e4
@farissahman1 <farissahman1@gmail.com>
2020-08-31 13:19:52 +0200

    make components smaller

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Homepage from "./components/Homepage";
+import Homepage from "./pages/homepage/Homepage";
 
 class App extends Component {
   render() {
